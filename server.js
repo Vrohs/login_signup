f6bb160fdfa93b90b93c28a8d31d045254644464
= <=>
2024-05-02 03:11:45 +0530

    Add new dependencies and update existing files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const model = require('./models/usermodel');
-const { name } = require('ejs');
+const cookieParser = require('cookie-parser');
 const port = 3000;
 
 
@@ -9,32 +9,43 @@ app.set('view engine', 'ejs');
 app.use(express.static('.'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
 
     res.render('login');
 })
 
 
-
-app.get('/create', async (req,res)=>{
+//  CRUD OPs
+app.get('/create', async (req, res) => {
 
     let createdUser = await model.UserModel.create({
 
-        username: "VIV",
-        password: "1556%$%^dbs"
+        username: "xyz sinha",
+        password: "sinha@981"
     })
 
     res.send(createdUser);
 })
 
-app.get('/update', async (req,res)=>{
+
+app.get('/read', async (req, res) => {
+
+    let readUser = await model.UserModel.find();
+
+    res.send(readUser);
+})
+
+
+app.get('/update', async (req, res) => {
+
 
     let updatedUser = await model.UserModel.findOneAndUpdate({
-    
-        username: "Vivek Rohtasvi", 
+
+        username: "Vivek Rohtasvi",
         password: "@rvHs536"
     });
 
@@ -42,6 +53,26 @@ app.get('/update', async (req,res)=>{
 })
 
 
+app.get('/delete', async (req, res) => {
+
+    let deletedUser = await model.UserModel.findOneAndDelete({ username: "xyz sinha" })
+
+    res.send(deletedUser);
+})
+
+
+
+// Auth
+
+app.get('/auth', (req, res)=>{
+
+    console.log(req.cookies);
+    res.cookie('name', 'viv');
+    res.send('done'); 
+})
+
+
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port http://localhost:${port}`);
 });
