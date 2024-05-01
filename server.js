2ae08ad299dbac1509f7ef420308a7aaf8d52ea8
= <=>
2024-05-02 00:16:23 +0530

    Add new dependencies and update existing files

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+
+app.set('view engine', 'ejs');
+app.use(express.static('.'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
